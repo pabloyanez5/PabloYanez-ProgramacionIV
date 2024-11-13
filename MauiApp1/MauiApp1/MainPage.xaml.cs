b222afe9bdf252856ca30a88426938419e7fc714
asd <130101518+pabloyanez5@users.noreply.github.com>
2024-11-13 09:00:52 -0500

    Segundo Progreso Maui

---
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void Boton1_DescendantAdded(object sender, ElementEventArgs e)
+        {
+            count = count + 3;
+            Boton1.Text = "El contador es: "+ count;
+        }
+    }
+
+}
